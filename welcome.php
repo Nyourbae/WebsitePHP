edf3565b674cb4f4d8dfe8ed8d731fae4e684475
Nyourbae <107290838+Nyourbae@users.noreply.github.com>
2022-11-29 08:18:22 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -47,9 +47,10 @@
           <?php echo $_POST["BarangPesanan"]; ?>
         </p>
           <?php
-          $servername = "localhost";
-          $username = "root";
-          $password = "";
+          $servername = "host";
+          $username = "user";
+          $password = "2e32e127681ce347874addc60994e6b8f89113e5e5d6685083b871d7b5c01ae8";
+	  $port = "5432";
           $dbname = "website";
           $Nama = $_POST["Nama"];
           $Email = $_POST["Email"];
@@ -96,5 +97,49 @@
   }
   ?>
 </body>
+	  <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try {
+  $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyDB.MyGuests");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
 
 </html>
